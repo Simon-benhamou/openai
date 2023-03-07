51f9e7b646d715b8161dfd781093a7f53943ecac
Simon Benhamou <simon@cemantica.com>
2023-03-07 10:12:17 +0200

    fix updates

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,23 +15,25 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
-    });
-    return;
-  }
-
+  const persona = req.body || '';
+  // if (animal.trim().length === 0) {
+  //   res.status(400).json({
+  //     error: {
+  //       message: "Please enter a valid animal",
+  //     }
+  //   });
+  //   return;
+  // }
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
-    });
-    res.status(200).json({ result: completion.data.choices[0].text });
+    const background = await getBackground(persona)
+    const quote = await getQuote(persona)
+    const image = await getPersonaImage(persona)
+    const initial =  persona.name.split(" ").map((val) => val.charAt(0)).slice(0, 2).join("")
+
+
+
+    const result = {...persona,background:background, quote: quote,initial:initial,image:image }
+    res.status(200).json({ result: result});
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
@@ -47,16 +49,103 @@ export default async function (req, res) {
     }
   }
 }
-
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+const getBackground = async (persona) => {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: generatePromptBackground(persona),
+    max_tokens:100,
+    temperature: 0.6,
+  });
+  return completion.data.choices[0].text
 }
+const getQuote = async (persona) => {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: generatePromptQuote(persona),
+    max_tokens:40,
+    temperature: 0.6,
+  });
+  return completion.data.choices[0].text
+}
+const getPersonaImage = async(persona) => {
+  const response = await openai.createImage({
+    prompt: `a face  picture  professional for marketing of ${persona.gender} of ${persona.age} years old`,
+    n: 1,
+    size: "512x512",
+  });
+  return  response.data.data[0].url;
+}
+
+/*   Needs: "A centralized way of being introduced to the new medical products on the market, as well as the research around each",
+  Expectation:"For pharma companies to provide relevant research when launching new medical products",
+  Frustration:"Having a variety of medical reps being scheduled in her day each introducing new medication, taking up consulting time. As well as attending product launches where marketing is the focus rather than the research and patient impact around the product",
+  personalityType:"Rational",
+  Quote:""The good physician treats the disease; the great physician treats the patient who has the disease." - William Osler"
+  Needs: "A simple way to locate medical records and contact the doctors holding these records",
+  Expectation:"Efficient processes with minimum waiting time between steps",
+  Frustration:"Waiting without knowing what's going on, not being in control of a process, shouts",
+  personalityType:"Guardian",
+  Quote:""A professional lawyer is one that controls all the aspects of a process in order to Win!
+  */
+function generatePromptBackground(persona) {
+
+  return`suggest me a background for a ${persona.gender} named ${persona.name} of ${persona.age} years old,  that works in ${persona.occupation} in ${persona.location}
+  without repeating the ${persona.name} is a ${persona.occupation} of ${persona.age} years old.
+    for Example :
+    suggest me a background for a  Female named Amelia Schmidt of 52 years old,  that works as a Psychiatrist in Germany would be :
+    Amelia is a practicing psychiatrist. She has many years of experience assisting patients of all ages suffering from various forms of depression. She is also a mother of 2 grown up children and continues to juggle the demands of being a parent and medical professional
+   ` 
+}
+function generatePromptQuote(persona) {
+  return`suggest me a quote that a ${persona.occupation} of ${persona.age} years old,  would say.
+    for Example :
+    suggest me a quote that a Psychiatrist of 52  years old,  would say would be :
+    The good physician treats the disease; the great physician treats the patient who has the disease." - William Osler
+   ` 
+}
+/*`base of the following userRequest object, build the following persona object with all its attribute including background and initial. 
+  the background is an example of the occupation and location and also the gender. 
+  userRequest : {
+    name:"Amelia Schmidt",
+    age:"52",
+    location:"Germany",
+    occupation:"Psychiatrist",
+    gender:"Female"
+    
+  },
+ persona:  {
+  name:"Amelia Schmidt",
+  age:"52",
+  location:"Germany",
+  initial:"AS"
+  occupation:"Psychiatrist",
+  gender:"Female"",
+  background:"Amelia is a practicing psychiatrist. She has many years of experience assisting patients of all ages suffering from various forms of depression. She is also a mother of 2 grown up children and continues to juggle the demands of being a parent and medical professional"
+
+  },
+  userRequest : {
+    name:"Dirk Bekker",
+    age:"35",
+    location:"Munich, Germany",
+    occupation:"Lawyer",
+    gender:"Male"
+    
+  },
+ persona:  {
+  name:"Dirk Bekker",
+  age:"35",
+  location:"Munich, Germany",
+  occupation:"Lawyer",
+  gender:"Male"
+  initial:"RR"
+  background:"Dirk is a lawyer in the field of health claims. He spends his day receiving requests of patients to sue institutions and performing at court"
+
+  },
+  userRequest: {
+    name: ${persona.name},
+    age:${persona.age}",
+    location:${persona.location},
+    occupation:${persona.occupation},
+    gender:${persona.gender}
+  },
+  persona:`;*/
--- pages/index.js
+++ pages/index.js
@@ -3,31 +3,52 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [persona, setPersona] = useState({
+    name: "",
+    occupation: "",
+    age:"",
+    location:"" ,
+    gender:""
 
+  });
+  const [result, setResult] = useState();
+  const [loading,setLoading] = useState(false)
   async function onSubmit(event) {
     event.preventDefault();
+    setLoading(true)
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify(persona),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
+
       }
 
+      console.log(data)
       setResult(data.result);
-      setAnimalInput("");
+      setLoading(false)
+
+      // setPersona({
+      //   name: "",
+      //   occupation: "",
+      //   age:"",
+      //   location:"" ,
+      //   genre:""
+    
+      // });
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
+      setLoading(false)
+
     }
   }
 
@@ -38,20 +59,64 @@ export default function Home() {
         <link rel="icon" href="/dog.png" />
       </Head>
 
-      <main className={styles.main}>
+      <main className={styles.main +" "+ styles.flex}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>My Persona Generator</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="name"
+            placeholder="Enter a persona name"
+            value={persona.name}
+            onChange={(e) => setPersona({...persona,name:e.target.value})}
           />
-          <input type="submit" value="Generate names" />
+          <input
+            type="text"
+            name="occupation"
+            placeholder="Enter a persona Occupation"
+            value={persona.occupation}
+            onChange={(e) => setPersona({...persona,occupation:e.target.value})}
+          />
+          <input
+            type="text"
+            name="age"
+            placeholder="Enter the age"
+            value={persona.age}
+            onChange={(e) => setPersona({...persona,age:e.target.value})}
+          />
+              <input
+            type="text"
+            name="genre"
+            placeholder="Enter the Genre"
+            value={persona.gender}
+            onChange={(e) => setPersona({...persona,gender:e.target.value})}
+          />
+                <input
+            type="text"
+            name="location"
+            placeholder="Enter the Location"
+            value={persona.location}
+            onChange={(e) => setPersona({...persona,location:e.target.value})}
+          />
+          <input type="submit" value="Generate persona" />
         </form>
-        <div className={styles.result}>{result}</div>
+        {loading &&  <div className={styles.spinner}></div> }
+        {result && !loading && <div className={styles.result}>
+          <div className={styles.profile}>
+            <img src={result?.image}/>
+            <div>
+              <div>{ `${result?.name}(${result?.initial})`}</div>
+              <div>{result?.occupation}</div>
+              <div>{result?.age}</div>
+              <div>{result?.location}</div>
+              <div>{result?.gender}</div>
+              </div> 
+            </div>
+          <div>{result?.background}</div>
+          <div>{result?.quote}</div>
+
+          
+          </div>}
       </main>
     </div>
   );
